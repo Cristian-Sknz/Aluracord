34014fdaf60d1e2fecc3e8b25b54f70496554b54
Cristian "SknZ" Ferreira <cristian.datingaa@gmail.com>
2022-01-25 22:19:30 -0300

    fix: corrigido problema com o pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
-const withPWA = require('next-pwa')
- 
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
+
 module.exports = withPWA({
   pwa: {
-    dest: 'public'
-  }
-})
+    dest: 'public',
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest.json$/],
+  },
+  reactStrictMode: true,
+});
